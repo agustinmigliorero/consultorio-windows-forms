12aa9fce2c58a47e409212180994a23940ce104d
compufoc <compufoc@gmail.com>
2024-11-07 16:50:30 -0300

    Formulario 3

---
+++ Consultorio-Medico/Form3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consultorio_Medico
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBoxnombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxapellido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxdni_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxtel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxtelfijo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxHisclin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePickerFechNaci_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxnotas_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCrearpaciente_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
