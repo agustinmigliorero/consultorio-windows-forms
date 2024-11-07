1dc2d892c29ece4e3d89dad6a4a603cfd71c4b7d
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2024-11-07 19:16:40 -0300

    Entidades de agenda y turno

---
+++ Entidades/Turno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Turno
+    {
+        public int IdTurno { get; set; }
+        public int IdPaciente { get; set; }
+        public int IdMedico { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool Cancelado { get; set; }
+        public string Estado { get; set; }
+
+        public Turno() { }
+
+        
+        public Turno(int idTurno, int idPaciente, int idMedico, DateTime fecha, bool cancelado, string estado)
+        {
+            IdTurno = idTurno;
+            IdPaciente = idPaciente;
+            IdMedico = idMedico;
+            Fecha = fecha;
+            Cancelado = cancelado;
+            Estado = estado;
+        }
+
+    }
+}
+
